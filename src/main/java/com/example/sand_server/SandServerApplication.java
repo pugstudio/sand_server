261a94108c9cf798345fcb028eefc5ca7b424fb5
imjjmoa <imjjver@naver.com>
2023-07-03 14:20:48 +0900

    first

---
+++ src/main/java/com/example/sand_server/SandServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.sand_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sand_server/SandServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sand_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
